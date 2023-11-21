ce25c6943a7f0dfac05ba9e13192ce2459f33d89
najmul sakib <najmus191@gmail.com>
2023-11-21 12:48:56 +0600

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,51 @@
+//fun main(args: Array<String>) {
+//  var name = "jamiul"
+//   name = "sakib "
+//  val last = "jamiul"
+//  print("my name is najmul sakib.")
+//  println("Thank you jamiul")
+//  println("my friend name is jamiul islam")
+//print("good day.\n")
+//  println("i love my cat")
+//sakib()
+//  print(name)
+//  println(last)
+//}
+//fun sakib(){
+//  var firstName = "Najmul"
+//  var lastName = "Sakib"
+//   println(firstName+ " " +lastName)
+//}
+
+
+fun main(args: Array<String>){
+//  float
+    val num1:Float = 14.6f
+    val num2:Float = 10.3f
+    println(num1+num2)
+
+//  Double
+    var num3:Double = 12.4
+    var num4:Double = 13.5
+    println(num3+num4)
+
+
+    var cha:Char = 'a'
+    var dec:Int = cha.toInt()
+    println(dec)
+
+    var isMarried:Boolean = true
+    println(isMarried)
+
+    var haveChild:Boolean = false
+    println(haveChild)
+
+    var ch = charArrayOf('a','b')
+    val st = String(ch)
+    println(st)
+
+    var name: String = "sakib"
+    println(name)
+
+
+}
---
+++ src/main/kotlin/dataType.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+    var num:Byte = 127
+    var num1:Byte = -127
+
+    var num2:Int = 233
+    var num3:Int = -2334
+    println(num3)
+    var a = Short.MAX_VALUE
+    var b = Int.MIN_VALUE
+    println(num2)
+    println("The max value is: ${a} and The minimum value is: ${b}")
+}
